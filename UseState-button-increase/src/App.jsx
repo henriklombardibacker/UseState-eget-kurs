c5523c21a90085fc4e92168375bda530dfac1f42
henriklombardibacker <117280176+henriklombardibacker@users.noreply.github.com>
2023-04-17 12:57:27 +0200

    Laget ferdig UseState kurset

---
+++ UseState-button-increase/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import IncreaseDecrease from './increaseDecrease'
+
+function App() {
+
+  return (
+    <div className="App">
+      <IncreaseDecrease />
+    </div>
+  )
+}
+
+export default App
